ec08f73d4e4c4e48567353b3a0747d8fb800d6ed
Ethan Windows <ethan.t.schmidt@gmail.com>
2018-09-17 15:29:22 -0400

    sdf

--- code/class2dplyr.R
+++ code/class2dplyr.R
@@ -28,5 +28,54 @@ diamonds %>% filter(carat >= 1 | cut == 'Ideal')
 
 diamonds %>% filter(cut == 'Ideal' | cut == 'Premium')
 
-# slect %in% from a vector
+# select %in% from a vector
 diamonds %>% filter(cut %in% c('Ideal', 'Premium'))
+
+# slicing through
+diamonds %>% slice(4:17)
+
+# slicing each individual rows as a vector
+diamonds %>% slice(c(4, 17))
+
+# MUTATE the dataframe
+diamonds %>% mutate(price / carat)
+
+diamonds %>% mutate(value=price / carat)
+
+# saving new tibble into variable
+diamonds <- diamonds %>% mutate(value=price / carat)
+
+# chaining verbs together
+diamonds %>% mutate(double=price*2) %>% mutate(new=sqrt(double))
+
+diamonds %>% mutate(double=price*2, new=sqrt(double))
+
+# functions & operations over whole tibble
+diamonds %>% summarize(mean(price))
+
+diamonds %>% summarize(AvgPrice=mean(price))
+
+summary <- diamonds %>% summarize(AvgPrice=mean(price), TotalSize=sum(carat))
+
+summary
+
+# grouping (like a pivot table)
+summary <- diamonds %>% 
+    group_by(cut) %>% 
+    summarize(AvgPrice=mean(price), TotalSize=sum(carat))
+
+summary
+
+# concatenating
+diamonds %>% 
+    filter(carat>1) %>% 
+    group_by(cut) %>% 
+    summarize(AvgSize=mean(carat), AvgPrice=mean(price)) %>%
+    arrange(-AvgPrice)
+
+# counting number of rows in 2 differnt ways
+diamonds %>%  
+    group_by(cut) %>% 
+    summarize(n()) # number of rows
+
+diamonds %>% count(cut)
